8b67fd4fbca3e4f95c0e1765565de8f9f9e348da
dinboyko <dinboyko@users.noreply.github.com>
2025-10-14 15:04:57 +0000

    deploy: b3863b8eac981b313fc68f995d401a21c32e4b11

--- feature/dina/_static/documentation_options.js
+++ feature/dina/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.3.2.dev12+g2d4485f09',
+    VERSION: '0.3.2.dev17+gb3863b8ea',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
